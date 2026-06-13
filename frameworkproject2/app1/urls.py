473bbca67ba617007cf942ebf3cde4a984f071be
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-06-14 00:05:01 +0530

    POST-api

--- frameworkproject2/app1/urls.py
+++ frameworkproject2/app1/urls.py
@@ -3,4 +3,5 @@ from app1 import views
 
 urlpatterns = [
     path('student/', views.get_student, name='get-student'),
+    path('addstudent', views.add_student, name='add-student'),
 ]
--- frameworkproject2/app1/views.py
+++ frameworkproject2/app1/views.py
@@ -3,6 +3,7 @@ from .models import Student
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from app1.serializers import StudentSerializers
+from rest_framework import status
 
 # Create your views here.
 @api_view(['GET'])
@@ -10,3 +11,11 @@ def get_student(request):
     students = Student.objects.all()
     serializer = StudentSerializers(students, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def add_student(request):
+    serializer = StudentSerializers(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
